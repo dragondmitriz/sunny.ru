205cdbcb0e8c8f268e8a8f19f5f259c6d952aea3
dragondmitriz <dmitriz.malyshko@mail.ru>
2019-09-02 20:46:32 +0300

    добавил текст программы "Развивающие игры"

--- lessons.php
+++ lessons.php
@@ -23,50 +23,55 @@
     <div class="lessons">
         <!--Ранее развитие-->
         <div class="lesson">
-            <div class="lesson-textblock">
-                <div class="lesson-name">Ранее развитие (1,6-2 лет)</div>
+                <h3 class="lesson-name">Ранее развитие (1,6-2 лет)</h3>
                 <div class="lesson-description"></div>
-            </div>
             <div class="lesson-image"></div>
         </div>
         <!--Развивающшие игры-->
         <div class="lesson">
-            <div class="lesson-textblock">
-                <div class="lesson-name">Развивающшие игры (2-4 лет)</div>
-                <div class="lesson-description"></div>
-            </div>
+                <h3 class="lesson-name">Развивающшие игры (2-4 лет)</h3>
+                <div class="lesson-description">
+                    Программа:
+                    <ol>
+                        <li>Начало занятий - контруирование</li>
+                        <li>Речевая игра</li>
+                        <li>Пальчиковые игры ("тренируем пальчики - развиваем речь", жестовые игры)</li>
+                        <li>Обучение счету в пределах 5-ти</li>
+                        <li>Знакомство с гласными буквами (10 букв) со стихами</li>
+                        <li>Дидактические игры на развитие моторики, мывшления, памяти, внимания, зщакрепление букв цветов, форм</li>
+                        <li>Физкультминутки в музеыкальном сопровождении</li>
+                        <li>Первыая пропись-=раскраска (учиимся правильно пользоваться уветными карандашами)</li>
+                        <li>Музыкальная часть: Психогимнастика, Ритмика, Песенки с погремушками</li>
+                        <li>Ручной труд (поделки из пластиолина, природногго материала, рисуем акварельными красками, аппликация без ножниц.)</li>
+                        <li>Музыкальный праздник "Новый год" (возможен второй музыкальный праздник в конце учебного года в мае)</li>
+                    </ol>
+                    Продолжительность занятий 40 мин. 2 раза в неделю. Первый месяц занятия проводятся вместе с родителями для адаптации ребенка.
+                    Далее занятия проводятсяя без родителей, при условии, если малыш позволит оставить его одного.
+                </div>
             <div class="lesson-image"></div>
         </div>
         <!--Подготовка к школе-->
         <div class="lesson">
-            <div class="lesson-textblock">
-                <div class="lesson-name">Подготовка к школе (5-7 лет)</div>
+                <h3 class="lesson-name">Подготовка к школе (5-7 лет)</h3>
                 <div class="lesson-description"></div>
-            </div>
             <div class="lesson-image"></div>
         </div>
         <!--Ритмопластика-->
         <div class="lesson">
-            <div class="lesson-textblock">
-                <div class="lesson-name">Ритмопластика (2-3 лет)</div>
+                <h3 class="lesson-name">Ритмопластика (2-3 лет)</h3>
                 <div class="lesson-description"></div>
-            </div>
             <div class="lesson-image"></div>
         </div>
         <!--Художественная лепка-->
         <div class="lesson">
-            <div class="lesson-textblock">
-                <div class="lesson-name">Художественная лепка (с 3-х лет и младшие школьники)</div>
+                <h3 class="lesson-name">Художественная лепка (с 3-х лет и младшие школьники)</h3>
                 <div class="lesson-description"></div>
-            </div>
             <div class="lesson-image"></div>
         </div>
         <!--Художественная лепка-->
         <div class="lesson">
-            <div class="lesson-textblock">
-                <div class="lesson-name">Художественная лепка (2-4 класс)</div>
+                <h3 class="lesson-name">Художественная лепка (2-4 класс)</h3>
                 <div class="lesson-description"></div>
-            </div>
             <div class="lesson-image"></div>
         </div>
     </div>
