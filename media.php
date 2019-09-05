43a822b21f922aa12b9f9a76a7031d44d2562732
dragondmitriz <dmitriz.malyshko@mail.ru>
2019-09-05 15:33:57 +0300

    добавил анимацию переключения на следующее изображение

--- media.php
+++ media.php
@@ -52,106 +52,130 @@
     var collectionGallery = [];
     var indx_selectedItem = 0;
 
-    $(document).ready(function () {
-        //обработка нажатия на изображение в галерее
-        $('.photo').click(function () {
-            let path_select_image = $(this).attr('src');//сохраняем адрес выбранного изображения
-            $('#gallery-image').css('background-image', 'url(' + path_select_image.slice(0, -4) + '!' + path_select_image.slice(-4) + ')');//установка выбранного изображения на блок отображения изображения в Галерее
-            collectionGallery.length = 0;//очистка коллекции адресов изображений высокого разрешения
-            //перебор все адресов изображений в блоке мероприятия,в котором находилось выбраное пользователем изображение, и создание коллекции с адресами изоражений более высокого разрешения
-            $(this).parent().children('.photo').each(function (indx, element) {
-                let url = $(element).attr('src');
-                if (url === path_select_image) {
-                    indx_selectedItem = indx;
-                }
-                url = url.slice(0, -4) + '!' + url.slice(-4);//форматирование адреса изображения для получения адреса к его более выскогокачесвенной копии, отмеченной восклицательным знаком в конце имени файла
-                collectionGallery[indx] = 'url(' + url + ')';
-            });
-            //присваивание адреса изображения для правого блока переключения на следующее изображение из коллекции адресов высококачественных изображений
-            if (indx_selectedItem === collectionGallery.length - 1) {
-                $('#gallery-button-right').css('background-image', collectionGallery[0]);
-            } else {
-                $('#gallery-button-right').css('background-image', collectionGallery[indx_selectedItem + 1]);
+    //обработка нажатия на изображение в галерее
+    $('.photo').click(function () {
+        let path_select_image = $(this).attr('src');//сохраняем адрес выбранного изображения
+        $('#gallery-image').css('background-image', 'url(' + path_select_image.slice(0, -4) + '!' + path_select_image.slice(-4) + ')');//установка выбранного изображения на блок отображения изображения в Галерее
+        collectionGallery.length = 0;//очистка коллекции адресов изображений высокого разрешения
+
+        //перебор все адресов изображений в блоке мероприятия,в котором находилось выбраное пользователем изображение, и создание коллекции с адресами изоражений более высокого разрешения
+        $(this).parent().children('.photo').each(function (indx, element) {
+            let url = $(element).attr('src');
+            if (url === path_select_image) {
+                indx_selectedItem = indx;
             }
-            //присваивание адреса изображения для левого блока переключения на следующее изображение из коллекции адресов высококачественных изображений
-            if (indx_selectedItem === 0) {
-                $('#gallery-button-left').css('background-image', collectionGallery[collectionGallery.length - 1]);
-            } else {
-                $('#gallery-button-left').css('background-image', collectionGallery[indx_selectedItem - 1]);
-            }
-            $('#gallery').fadeIn(400);//проявление "Галереи"
+            url = url.slice(0, -4) + '!' + url.slice(-4);//форматирование адреса изображения для получения адреса к его более выскогокачесвенной копии, отмеченной восклицательным знаком в конце имени файла
+            collectionGallery[indx] = 'url(' + url + ')';
         });
-        //обработка нажатия на кнопку закрытия Галереи
-        $('#gallery-button-close').click(function () {
-            $("#gallery").fadeOut(400);
+
+        //присваивание адреса изображения для правого блока переключения на следующее изображение из коллекции адресов высококачественных изображений
+        if (indx_selectedItem === collectionGallery.length - 1) {
+            $('#gallery-button-right').css('background-image', collectionGallery[0]);
+        } else {
+            $('#gallery-button-right').css('background-image', collectionGallery[indx_selectedItem + 1]);
+        }
+
+        //присваивание адреса изображения для левого блока переключения на следующее изображение из коллекции адресов высококачественных изображений
+        if (indx_selectedItem === 0) {
+            $('#gallery-button-left').css('background-image', collectionGallery[collectionGallery.length - 1]);
+        } else {
+            $('#gallery-button-left').css('background-image', collectionGallery[indx_selectedItem - 1]);
+        }
+        $('#gallery').fadeIn(400);//проявление "Галереи"
+    });
+
+    //обработка нажатия на кнопку закрытия Галереи
+    $('#gallery-button-close').click(function () {
+        $("#gallery").fadeOut(400);
+    });
+
+    //обработка нажатия на левое изображения в просмотре Галереи
+    /*проверяет позицию текущего изображения в массиве коллекции адресов
+    * смещает соответсвующе индекс текущего изображения
+    * и присваивает новые изображения для боковых блоков навигации*/
+    $('#gallery-button-left').click(function () {
+
+        //загрузка нового изображения в "призрака"
+        /*"призрак" - блок с шириной 0 в начале контейнера "Галереи"(слева)
+        * отвечает за анимацию блоков при переключении картинок
+        * позволяет расширить коллекцию оперируемых изображений на 1,
+        * чтобы полноценно отображать анимацию с новым изображением*/
+        if (indx_selectedItem - 2 < 0) {
+            $('#ghoast').css('background-image', collectionGallery[collectionGallery.length + indx_selectedItem - 3]);
+        } else {
+            $('#ghoast').css('background-image', collectionGallery[indx_selectedItem - 2]);
+        }
+        $('#ghoast').attr('class', 'gallery-button');//для выравниваниия расположения изображений до и после анимации, так как кнопки галереи имеют отступы в 5px, что выдаёт "призрака" и тот выдаёт своё существование
+
+        //анимация переключения изображений
+        let speedAnimation = 400;
+        $('#gallery-button-right').animate({width: '0'}, speedAnimation / 2);
+        $('#gallery-button-left').animate({width: '60%'}, speedAnimation);
+        $('#gallery-image').animate({width: '20%'}, speedAnimation);
+        $('#ghoast').animate({width: '20%'}, speedAnimation, function () {
+
+            //"Квантовая телепортация" после анимации
+            $('#gallery-button-right')
+                .css('width', '20%')
+                .css('background-image', $('#gallery-image').css('background-image'));
+            $('#gallery-image')
+                .css('width', '60%')
+                .css('background-image', $('#gallery-button-left').css('background-image'));
+            $('#gallery-button-left')
+                .css('width', '20%')
+                .css('background-image', $('#ghoast').css('background-image'));
+            $('#ghoast')
+                .css('width', '0')
+                .css('background-image', '')
+                .attr('class', '');//делаем призрака призраком)
         });
-        //обработка нажатия на левое изображения в просмотре Галереи
-        /*проверяет позицию текущего изображения в массиве коллекции адресов
-        * смещает соответсвующе индекс текущего изображения
-        * и присваивает новые изображения для боковых блоков навигации*/
-        $('#gallery-button-left').click(function () {
-            //загрузка нового изображения в "призрака"
-            /*"призрак" - блок с шириной 0 в начале контейнера "Галереи"(слева)
-            * отвечает за анимацию блоков при переключении картинок
-            * позволяет расширить коллекцию оперируемых изображений на 1,
-            * чтобы полноценно отображать анимацию с новым изображением*/
-            if (indx_selectedItem - 2 < 0) {
-                $('#ghoast').css('background-image', collectionGallery[collectionGallery.length + indx_selectedItem - 3]);
-            } else {
-                $('#ghoast').css('background-image', collectionGallery[indx_selectedItem - 2]);
-            }
-            $('#ghoast').attr('class', 'gallery-button');//для выравниваниия расположения изображений до и после анимации, так как кнопки галереи имеют отступы в 5px, что выдаёт "призрака" и тот выдаёт своё существование
-            //анимация переключения изображений
-            let speedAnimation = 400;
-            $('#gallery-button-right').animate({width: '0'}, speedAnimation);
-            $('#gallery-button-left').animate({width: '60%'}, speedAnimation);
-            $('#gallery-image').animate({width: '20%'}, speedAnimation);
-            $('#ghoast').animate({width: '20%'}, speedAnimation, function () {
-                //"Квантовая телепортация" после анимации
-                let rightImage = $('#gallery-image').css('background-image');
-                let mainImage = $('#gallery-button-left').css('background-image');
-                let leftImage = $('#ghoast').css('background-image');
-                $('#gallery-button-right')
-                    .css('width', '20%')
-                    .css('background-image', rightImage);
-                $('#gallery-image')
-                    .css('width', '60%')
-                    .css('background-image', mainImage);
-                $('#gallery-button-left')
-                    .css('width', '20%')
-                    .css('background-image', leftImage);
-                $('#ghoast')
-                    .css('width', '0')
-                    .css('background-image', '')
-                    .attr('class', '');//делаем призрака призраком)
-            });
-            //вычисление индекса нового основного изображения
-            if (indx_selectedItem === 0) {
-                indx_selectedItem = collectionGallery.length - 1;
-            } else {
-                indx_selectedItem--;
-            }
-        });
-        //обработка нажатия на правое изображение в просмотре Галереи
-        /*проверяет позицию текущего изображения в массиве коллекции адресов
-        * смещает соответсвующе индекс текущего изображения
-        * и присваивает новые изображения для боковых блоков навигации*/
-        $('#gallery-button-right').click(function () {
-            if (indx_selectedItem === collectionGallery.length - 1) {
-                indx_selectedItem = 0;
-                $('#gallery-button-left').css('background-image', collectionGallery[collectionGallery.length - 1]);
-                $('#gallery-button-right').css('background-image', collectionGallery[1]);
-            } else {
-                indx_selectedItem++;
-                $('#gallery-button-left').css('background-image', collectionGallery[indx_selectedItem - 1]);
-                if (indx_selectedItem === collectionGallery.length - 1) {
-                    $('#gallery-button-right').css('background-image', collectionGallery[0]);
-                } else {
-                    $('#gallery-button-right').css('background-image', collectionGallery[indx_selectedItem + 1]);
-                }
-            }
-            $('#gallery-image').css('background-image', collectionGallery[indx_selectedItem]);
-        });
+
+        //вычисление индекса нового основного изображения
+        if (indx_selectedItem === 0) {
+            indx_selectedItem = collectionGallery.length - 1;
+        } else {
+            indx_selectedItem--;
+        }
+    });
+
+    //обработка нажатия на правое изображение в просмотре Галереи
+    /*проверяет позицию текущего изображения в массиве коллекции адресов
+    * смещает соответсвующе индекс текущего изображения
+    * и присваивает новые изображения для боковых блоков навигации*/
+    $('#gallery-button-right').click(function () {
+
+        //"Квантовая телепортация"
+        $('#ghoast')
+            .css('width', '20%')
+            .css('background-image', $('#gallery-button-left').css('background-image'))
+            .attr('class', 'gallery-button');
+        $('#gallery-button-left')
+            .css('width', '60%')
+            .css('background-image', $('#gallery-image').css('background-image'));
+        $('#gallery-image')
+            .css('width', '20%')
+            .css('background-image', $('#gallery-button-right').css('background-image'));
+        $('#gallery-button-right')
+            .css('width', '0');
+        if (indx_selectedItem + 2 > collectionGallery.length - 1) {
+            $('#gallery-button-right').css('background-image', collectionGallery[indx_selectedItem + 1 - collectionGallery.length]);
+        } else {
+            $('#gallery-button-right').css('background-image', collectionGallery[indx_selectedItem + 2]);
+        }
+
+        //анимация переключения изображений
+        let speedAnimation = 400;
+        $('#ghoast').animate({width: '0'}, speedAnimation / 2);
+        $('#gallery-button-left').animate({width: '20%'}, speedAnimation);
+        $('#gallery-image').animate({width: '60%'}, speedAnimation);
+        $('#gallery-button-right').animate({width: '20%'}, speedAnimation);
+
+        //вычисление индекса нового основного изображения
+        if (indx_selectedItem === collectionGallery.length - 1) {
+            indx_selectedItem = 0;
+        } else {
+            indx_selectedItem++;
+        }
     });
 </script>
 </body>
