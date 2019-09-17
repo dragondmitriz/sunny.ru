59c271499157b7b72fd9d678a6751da4040148d4
dragondmitriz <dmitriz.malyshko@mail.ru>
2019-09-17 13:29:31 +0300

    добавил в head счетчик для Яндекс.Метрики

--- contacts.php
+++ contacts.php
@@ -1,5 +1,27 @@
 <html>
 <head>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (m, e, t, r, i, k, a) {
+            m[i] = m[i] || function () {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(55363147, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true
+        });
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/55363147" style="position:absolute; left:-9999px;" alt=""/></div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>Контакты</title>
     <link href="css/layout.css" rel="stylesheet"/>
